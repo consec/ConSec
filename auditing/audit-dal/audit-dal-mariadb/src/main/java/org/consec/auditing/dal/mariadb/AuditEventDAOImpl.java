d91dcf494ec8d013c7fd2e0280ed7c35701b1024
Damjan Murn <damjan.murn@xlab.si>
2014-07-04 15:50:09 +0200

    Excluded ca-server from compile.

---
+++ auditing/audit-dal/audit-dal-mariadb/src/main/java/org/consec/auditing/dal/mariadb/AuditEventDAOImpl.java
@@ -0,0 +1,16 @@
+package org.consec.auditing.dal.mariadb;
+
+import org.consec.auditing.common.AuditEvent;
+import org.consec.auditing.common.dal.AuditEventDAO;
+
+public class AuditEventDAOImpl implements AuditEventDAO {
+    @Override
+    public void save(AuditEvent auditEvent) {
+
+    }
+
+    @Override
+    public void retrieve(String s) {
+
+    }
+}
